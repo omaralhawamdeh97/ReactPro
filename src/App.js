65b3dbd91f80f4ab7e6c2413b02f37000ac77356
aliigraibe <aliigraibe93@gmail.com>
2021-06-06 10:05:16 +0300

    Tsk Four

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import React, { useState } from "react";
 import {ThemeProvider} from "styled-components"
-import { Head, ImageSize,GlobalStyle} from "./Styles"
+import { Head,ThemeButton, ImageSize,GlobalStyle} from "./Styles"
 import ProductList from "./component/ProductList"
 import Home from "./component/Home"
 import './App.css';
- const theme=  {
-  mainColor: "#242424",   
-  backgroundColor: "#fefafb",
-  
-}
+
+
+const theme = {
+  light: {
+    mainColor: "#242424", // main font color
+    backgroundColor: "#fefafb", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+  dark: {
+    mainColor: "#fefafb", // main font color
+    backgroundColor: "#242424", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+};
+
 
 function App() {
 
-  return (<ThemeProvider theme={theme}>
+  const  [color, setcolor] = useState(theme.light);
+    const swit =( )=>{
+
+  if (color===theme.light) setcolor(theme.dark )
+   else setcolor(theme.light )
+}
+  return (<ThemeProvider theme={color}>
     
   
       <GlobalStyle/>
       <Head>
+
+      <ThemeButton onClick= {(swit)} >
+        Dark Theme
+      </ThemeButton>
        <Home/>
        <ImageSize alt = "phon" src="https://static.toiimg.com/photo/71335454.cms"/>
       </Head>
--- src/Styles.js
+++ src/Styles.js
@@ -7,14 +7,20 @@ body {
   }
 `
 
-
-  
+  export const ThemeButton = styled.button`
+  font-size: 1em;
+  margin: 1.25em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.mainColor};
+  color: ${(props) => props.theme.backgroundColor};
+`;
 export const Flxii = styled.div `
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-margin-top:5px;
+margin-top:10px;
 
 `;
 export const ImageSize = styled.img `
@@ -24,15 +30,13 @@ export const ImageSize = styled.img `
 
     export const Head = styled.head
 `
-
-    background-color: #282c34;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     font-size: calc(10px + 2vmin);
-    color: white;
+    color: black;
 
     `
 
